15ea626adcd6cd80f2793533242375a678c5c0fd
codeninja912 <ppak@marketaxess.com>
2025-03-21 13:47:06 -0400

    Update command_injection.java

--- command_injection.java
+++ command_injection.java
@@ -1,7 +1,7 @@
 import java.io.*;
 import java.util.*;
 
-public class command_injection{ 
+public class command_injection{  
 public static void main(String[] args){ 
 Scanner sc=new Scanner(System.in); 
 System.out.println("Enter Username"); 
